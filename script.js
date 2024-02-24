c19a0279cdaf887441f0fe42962452d368a57192
Kunaal Desai <kunaaldesai13@gmail.com>
2024-02-24 16:27:51 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleThemeButton = document.getElementById("toggleTheme");
+
+  // Load theme from local storage or default to 'light'
+  let currentTheme = localStorage.getItem('theme') || 'light';
+  updateTheme();
+
+  toggleThemeButton.addEventListener('click', () => {
+    currentTheme = (currentTheme === 'light' ? 'dark' : 'light');
+    updateTheme();
+  });
+
+  function updateTheme() {
+    const bodyElement = document.body;
+    const containerElement = document.querySelector('.container-fluid');
+
+    if (currentTheme === 'dark') {
+      bodyElement.classList.add('dark-mode');
+      containerElement.classList.add('dark-mode');
+      toggleThemeButton.classList.remove('btn-secondary');
+      toggleThemeButton.classList.add('btn-dark');
+    } else {
+      bodyElement.classList.remove('dark-mode');
+      containerElement.classList.remove('dark-mode');
+      toggleThemeButton.classList.remove('btn-dark');
+      toggleThemeButton.classList.add('btn-secondary');
+    }
+    localStorage.setItem('theme', currentTheme);
+  }
+});
